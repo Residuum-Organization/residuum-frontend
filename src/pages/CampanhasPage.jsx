4b8ca9111831e4c6bb2c1a582dbb05351bd3293b
Jonas Santos <jonas.santos2302@gmail.com>
2026-06-20 17:11:14 -0400

    Fix import path for CampaignLayout component
    fix: corrige import de CampaignLayout em CampanhaDetalhesPage

--- src/pages/CampanhasPage.jsx
+++ src/pages/CampanhasPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import CampaignLayout, {
   LogoResiduum,
-} from "../components/campanhas/CampaignLayout";
+} from "../components/Campanhas/CampaignLayout";
 
 const campanhasPadrao = [
   {
